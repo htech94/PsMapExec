dde6009cd9ff79cf1f6c307cdb51e009053d42d5
The-Viper-One <68926315+The-Viper-One@users.noreply.github.com>
2023-11-11 19:50:23 +0000

    Update PsMapExec.ps1

--- PsMapExec.ps1
+++ PsMapExec.ps1
@@ -102,7 +102,7 @@ Write-Output $Banner
 Write-Host "Github  : "  -NoNewline
 Write-Host "https://github.com/The-Viper-One"
 Write-Host "Version : " -NoNewline
-Write-Host "0.4.2"
+Write-Host "0.4.3"
 Write-Host
 
 # If no targets have been provided
