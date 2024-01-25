bd6d0f113573c91b944e4036229881c9e4f47914
The-Viper-One <68926315+The-Viper-One@users.noreply.github.com>
2024-01-25 22:01:57 +0000

    Update PsMapExec.ps1

--- PsMapExec.ps1
+++ PsMapExec.ps1
@@ -1,96 +1,205 @@
 Function PsMapExec {
-
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Default')]
     Param(
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Command = '',
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Command,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Targets = '',
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Targets,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [String]$Domain = "$env:USERDNSDOMAIN",
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Username = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Username,
 
-        [Parameter(Mandatory = $True, ValueFromPipeline = $true)]
-        [String]$Method = "",
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Method,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Module = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Module,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Hash = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Hash,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Password = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Password,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$UserDomain = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$UserDomain,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$LocalFileServer = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$LocalFileServer,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [int]$Threads = 30,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$Force,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$LocalAuth,
     
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$CurrentUser = $True,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$SuccessOnly,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$ShowOutput,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$Ticket = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$Ticket,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [Switch]$AccountAsPassword,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [Switch]$EmptyPassword,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [int]$Port = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [int]$Port,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [Switch]$NoParse,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$SprayHash = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$SprayHash,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [String]$SprayPassword = "",
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [String]$SprayPassword,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$NoBanner,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [string]$DomainController,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [int]$Timeout = 3000,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$Flush,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
         [switch]$Scramble,
 
-        [Parameter(Mandatory = $False, ValueFromPipeline = $true)]
-        [switch]$Rainbow
+        [Parameter(Mandatory = $False, ValueFromPipeline = $true, ParameterSetName = 'Default')]
+        [switch]$Rainbow,
+
+        [Parameter(Mandatory = $False, ParameterSetName = 'Help')]
+        [Switch]$Help
     )
 
+    if ($Help) {
 
+        $HelpOutput = @("
+
+-----------------------------------------------------------------------------------------------------------
+---------------------------------------------- [ Help Menu ] ---------------------------------------------- 
+
+Documentation: https://viperone.gitbook.io/pentest-everything/psmapexec
+
+-----------------------------------------------------------------------------------------------------------
+ Examples
+
+ # Execute WMI commands over all systems in the domain using password authentication
+   PsMapExec -Targets all -Method WMI -Username Admin -Password Pass -Command ""net user""
+
+ # Execute WinRM commands over all systems in the domain using hash authentication
+   PsMapExec -Targets all -Method WinRM -Username Admin -Hash [Hash] -Command ""net user""
+
+ # Check RDP Access against workstations in the domain and using local authentication
+   PsMapExec -Targets Workstations -Method RDP -Username LocalAdmin -Password Pass -LocalAuth
+
+ # Dump SAM on a single system using SMB and a -ticket for authentication
+   PsMapExec -Targets DC01.Security.local -Method SMB -Ticket [Base64-Ticket] -Module SAM
+
+ # Check SMB Signing on all domain systems
+   PsMapExec -Targets All -Method GenRelayList
+
+ # Dump LogonPasswords on all Domain Controllers over WinRM
+   PsMapExec -Targets DCs -Method WinRM -Username Admin -Password Pass -Module LogonPasswords
+
+ # Use WMI to check current user admin access from systems read from a text file
+   PsMapExec -Targets C:\temp\Systems.txt -Method WMI
+
+ # Spray passwords across all accounts in the domain
+   PsMapExec -Method Spray -SprayPassword [Password]
+
+ # Spray Hashes across all accounts in the domain
+   PsMapExec -Method Spray -SprayHash [Hash]
+
+ # Spray Hashes across all Domain Admin group users
+   PsMapExec -Targets ""Domain Admins"" -Method Spray -SprayHash [Hash]
+
+-----------------------------------------------------------------------------------------------------------
+
+ 
+ 
+ General Usage Parameters
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | Parameter           |    Value     | Description                                                     |
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | -Command            |    whoami    | Executes a command on the target system                         |
+ | -CurrentUser        |     N/A      | Defaults to current user context without other credentials      |
+ | -Domain             |    Domain    | Sets the domain for the operation, defaults to user's domain    |
+ | -DomainController   |      DC      | Identifies which Domain controller to authenticate against      |
+ | -Force              |     N/A      | Forces execution with domain/admin credentials                  |
+ | -Flush              |     N/A      | Clears LDAP variables for long-term, large environments         |
+ | -LocalFileServer    |      IP      | Retrieves scripts from a specified local file server IP         |
+ | -Module             |    Module    | Specifies the module for command execution                      |
+ | -NoBanner           |     N/A      | Hides the script banner                                         |
+ | -NoParse            |     N/A      | Avoids parsing some module outputs                              |
+ | -Rainbow            |     N/A      | Uses online rainbow tables for specific module hash analysis    |
+ | -SuccessOnly        |     N/A      | Shows only successful results                                   |
+ | -Timeout            |     int      | Sets the port scan timeout in milliseconds                      |
+ | -Threads            |     int      | Determines concurrent job count, default is 30                  |
+ +---------------------+--------------+-----------------------------------------------------------------+
+
+ Authentication Parameters
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | Parameter           |    Value     | Description                                                     |
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | -Hash               | RC4 or AES   | Hash value. Must be supplied with -Username                     |
+ | -LocalAuth          |     N/A      | Specifies local account authentication                          |
+ | -Password           |   Password   | Password value. Must be supplied with -Username                 |
+ | -Ticket             |    Ticket    | B64 encoded Kerberos ticket for authentication.                 |
+ +---------------------+--------------+-----------------------------------------------------------------+
+
+ Command execution Parameters
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | Parameter           |    Value     | Description                                                     |
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | -Command            |   Command    | Runs the specified command on the remote system                 |
+ | -Module             |   Module     | Specifies the module for command execution                      |
+ | -ShowOutput         |     N/A      | Displays output for executed modules. Commands still shown      |
+ +---------------------+--------------+-----------------------------------------------------------------+
+
+ Spraying Parameters
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | Parameter           |    Value     | Description                                                     |
+ +---------------------+--------------+-----------------------------------------------------------------+
+ | -AccountAsPassword  |      N/A     | Sprays SAM Account names as passwords                           |
+ | -EmptyPassword      |      N/A     | Sprays blank passwords                                          |
+ | -SprayHash          | RC4/AES/NTLM | Used for hash spraying                                          |
+ | -SprayPassword      |   Password   | Used for password spraying                                      |
+ +---------------------+--------------+-----------------------------------------------------------------+
+
+
+ ---------------------------------------------- [ End Menu ] ----------------------------------------------- 
+ -----------------------------------------------------------------------------------------------------------
+
+
+
+
+")
+
+        $HelpOutput | Write-host
+        return
+
+    }
 
     $startTime = Get-Date
     Set-Variable MaximumHistoryCount 32767
@@ -505,7 +614,7 @@ Version : 0.4.7")
                         try {
                             Write-Verbose "Attempting to obtain current user ticket"
                             if ($DomainController -ne "") {
-                                $BaseTicket = Invoke-rTickets send /domain:$domain /dc:$DomainController /nowrap| Out-String
+                                $BaseTicket = Invoke-rTickets send /domain:$domain /dc:$DomainController /nowrap | Out-String
                             }
                             else {
                                 $BaseTicket = Invoke-rTickets send /nowrap /domain:$domain | Out-string
